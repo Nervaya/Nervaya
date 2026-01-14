9e5dd6860968303bc78a2e9b9a5147ad0b9dbda9
VenkatAillaboina <vaillaboina@gmail.com>
2026-01-14 17:13:33 +0530

    builded the support us page with chat with us component

---
+++ src/app/support/page.tsx
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import ChatWithUs from "@/components/Support/ChatWithUs";
+import AboutUsConsultation from "@/components/AboutUS/AboutUsConsultation";
+import styles from "./support.module.css";
+
+const SupportPage = () => {
+    return (
+        <div className={styles.container}>
+            <Navbar />
+            <div className={styles.header}>
+                <h1 className={styles.title}>Welcome to the Support Page</h1>
+                <p className={styles.subtitle}>
+                    We're here to help you with any problems you may be having with our product.
+                </p>
+            </div>
+            <ChatWithUs />
+            {/* frequently asked questions component */}
+            <AboutUsConsultation />
+        </div>
+    );
+};
+
+export default SupportPage;
