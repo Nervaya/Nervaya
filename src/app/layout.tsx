68e4ae75f939bee9da41c3ee5e2b7a70facaf142
kokkondaBhanuteja <bhanutejakokkonda@gmail.com>
2026-03-10 00:22:25 +0530

    style: Remove unnecessary empty lines within the GTM integration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,8 +47,8 @@ export default function RootLayout({
       <head>
         {gtmId && (
           <>
-            { }
-            { }
+            {}
+            {}
             <script
               dangerouslySetInnerHTML={{
                 __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -63,7 +63,6 @@ export default function RootLayout({
       </head>
       <body className={`${sourceSans.variable} ${merriweather.variable} ${geistMono.variable}`}>
         {gtmId && (
-           
           <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
