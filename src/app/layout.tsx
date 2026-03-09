5c0d943b5f9c019015c591c666c5a8adfd622e3a
kokkondaBhanuteja <bhanutejakokkonda@gmail.com>
2026-03-09 21:54:04 +0530

    feat: integrate Google Tag Manager and refresh UI styling for cart components and page headers.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,30 @@ export default function RootLayout({
 
   return (
     <html lang="en" className={`${sourceSans.variable} ${merriweather.variable} ${geistMono.variable}`}>
+      <head>
+        {gtmId && (
+          <>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${gtmId}');`,
+              }}
+            />
+          </>
+        )}
+      </head>
       <body className={`${sourceSans.variable} ${merriweather.variable} ${geistMono.variable}`}>
+        {gtmId && (
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+        )}
         <Providers>
           <BodyRouteClass />
           <EngagementTracker />
